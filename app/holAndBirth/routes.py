2c9948dff035e8d98d7a7b22ced7c23be0aebd94
BorderWarner <147915786+BorderWarner@users.noreply.github.com>
2025-01-06 00:21:43 +0300

    fix limit get_bAndH_details

--- app/holAndBirth/routes.py
+++ app/holAndBirth/routes.py
@@ -42,6 +42,6 @@ def init_socketio_hab(app):
     def get_bAndH_details():
         try:
             socketio.emit('holidays_and_birthdays_details_update',
-                          {'holidays': get_holidays_for(limit=25), 'birthdays': get_birthdays_for(limit=25)})
+                          {'holidays': get_holidays_for(limit=23), 'birthdays': get_birthdays_for(limit=23)})
         except Exception as e:
             socketio.emit('error', f"Ошибка: {e}")
