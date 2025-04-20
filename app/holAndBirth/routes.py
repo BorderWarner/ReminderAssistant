8638f64dce6122a03d3c6c65110300d7108ee97c
BorderWarner <147915786+BorderWarner@users.noreply.github.com>
2025-04-20 12:11:56 +0300

    min fix TEST

--- app/holAndBirth/routes.py
+++ app/holAndBirth/routes.py
@@ -26,6 +26,7 @@ def init_socketio_hab(app):
 
     @socketio.on('get_birthdays')
     def get_birthdays():
+        print(111111)
         try:
             socketio.emit('birthdays_update', get_birthdays_for(days=30, limit=10))
         except Exception as e:
@@ -33,6 +34,7 @@ def init_socketio_hab(app):
 
     @socketio.on('get_holidays')
     def get_holidays():
+        print(222222)
         try:
             socketio.emit('holidays_update', get_holidays_for(days=30, limit=10))
         except Exception as e:
@@ -41,6 +43,7 @@ def init_socketio_hab(app):
     @socketio.on('get_bAndH_details')
     def get_bAndH_details():
         try:
+            print(33333)
             socketio.emit('holidays_and_birthdays_details_update',
                           {'holidays': get_holidays_for(limit=23), 'birthdays': get_birthdays_for(limit=23)})
         except Exception as e:
